b7a8650c131ccd422eb2489d4c6e377c6b541c34
Marco Zangari <mjzangari@gmail.com>
2017-12-07 17:40:08 -0800

    added changes

---
+++ src/test_insertion_sort.py
@@ -0,0 +1,12 @@
+"""Test the insertion sort."""
+import pytest
+
+from insertion_sort import insertion_sort
+
+
+def insertion_sort_big_list():
+    """Insert a big list in insertion sort."""
+
+
+def insertion_sort_small_list():
+    """Insert a small list in insertion sort."""
