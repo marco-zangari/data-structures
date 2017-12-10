40742270be9ac40e7327a9ba8fb965ec58712720
Marco Zangari <mjzangari@gmail.com>
2017-12-10 11:12:39 -0800

    added if name main line for running from command line

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -12,3 +12,32 @@ def insertion_sort(list_obj):
             i = i - 1
             list_obj[i] = current
     return list_obj
+
+if __name__ == '__main__':
+    import timeit as ti
+
+    a_list = [4, 3, 7, 6]
+    b_list = [72, 4, 10, 6, 20, 18, 91, 45, 3, 15]
+    c_list = [list(range(100, 0, -1))]
+
+    time_a = ti.timeit("insertion_sort(a_list)",
+    setup = "from __main__ import a_list, insertion_sort")
+
+    time_b = ti.timeit("insertion_sort(b_list)",
+    setup = "from __main__ import b_list, insertion_sort")
+
+    time_c = ti.timeit("insertion_sort(c_list)",
+    setup = "from __main__ import c_list, insertion_sort")
+
+    print(f"""
+These are insertion sort times for different kinds of lists:\
+one short, one longer, and one longer still.
+
+Input: [4, 3, 7, 6]
+Output: { time_a }
+
+Input: [72, 4, 10, 6, 20, 18, 91, 45, 3, 15]
+Output: { time_b }
+
+Input (list(range(100, 0, -1)))
+Output: { time_c }.""")
