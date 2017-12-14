a2fbceb36440f4f27b8117f83a225bc8a47bb86b
Marco Zangari <mjzangari@gmail.com>
2017-12-13 16:58:27 -0800

    added traversals. started tests.

---
+++ src/test_bst_traversal.py
@@ -0,0 +1,8 @@
+"""Test four binary search tree traversal methods."""
+
+import pytest
+
+
+def test_bst_pre_order_traversal():
+    """Test bst pre order traversal."""
+    from bst_traversal import bst_pre_order_traversal
