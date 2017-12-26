0da3ac0739f1976c8eacc7f0a730c940a71665b4
Marco Zangari <mjzangari@gmail.com>
2017-12-26 13:58:33 -0500

    working through sortition

--- src/radix.py
+++ src/radix.py
@@ -4,12 +4,12 @@
 def radix_sort(unsorted):
     """Sorts unsorted numbers in list working right- to left-most digit."""
     holder_list = []
-    import pdb; pdb.set_trace()
     nth_place = len(str(max(unsorted)))
     if nth_place < 10:
         str_nth = '0' + str(nth_place)
     for num in unsorted:
-        format((num), str_nth)
-    holder_list.append(num)
+        reformatted_num = format((num), str_nth)
+        import pdb; pdb.set_trace()
+        holder_list.append(reformatted_num)
 
 unsorted = [170, 45, 75, 90, 2, 802, 2, 66]
