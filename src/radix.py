30b57f3cb3aed89a6de4c63afddd2a99ac215816
Marco Zangari <mjzangari@gmail.com>
2017-12-26 17:03:53 -0500

    working through sortition

--- src/radix.py
+++ src/radix.py
@@ -7,12 +7,28 @@ def radix_sort(unsorted, base=10):
     temp_list = []
     buckets = [[] for _ in range(base)]
     nth_place = len(str(max(unsorted)))
+    iteration = base ** (nth_place - 1)
     if nth_place < 10:
         str_nth = '0' + str(nth_place)
+    else:
+        str_nth = str(nth_place)
     for num in unsorted:
         reformatted_num = format((num), str_nth)
-        holder_list.append(reformatted_num)
+        holder_list.append(reformatted_num) #  all numbers same length, stringified with zeros in front
 
+    nth_place = int(nth_place)
+    while int(nth_place) > 0:
+
+        for num in holder_list:
+            nearly_sort = num // (base ** (int(nth_place) - 1)
+            placement = str(nearly_sort)[-1]
+            buckets[int(placement) - 1].append(num)
+
+            for bucket in buckets:
+
+            holder_list = []
+
+        int(nth_place) -= 1
 
 
 unsorted = [170, 45, 75, 90, 2, 802, 2, 66]
