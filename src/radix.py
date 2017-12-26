781437109378af0001bc14f86da1788d9ae6d5aa
Marco Zangari <mjzangari@gmail.com>
2017-12-26 14:09:08 -0500

    working through sortition

--- src/radix.py
+++ src/radix.py
@@ -1,15 +1,18 @@
 """Implement radix sort function."""
 
 
-def radix_sort(unsorted):
+def radix_sort(unsorted, base=10):
     """Sorts unsorted numbers in list working right- to left-most digit."""
     holder_list = []
+    temp_list = []
+    buckets = [[] for _ in range(base)]
     nth_place = len(str(max(unsorted)))
     if nth_place < 10:
         str_nth = '0' + str(nth_place)
     for num in unsorted:
         reformatted_num = format((num), str_nth)
-        import pdb; pdb.set_trace()
         holder_list.append(reformatted_num)
+
+
 
 unsorted = [170, 45, 75, 90, 2, 802, 2, 66]
