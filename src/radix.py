ba4ed48bd1d20b3eb9227bff6b4644aa7a5008c1
Marco Zangari <mjzangari@gmail.com>
2017-12-28 13:42:25 -0500

    added to function

--- src/radix.py
+++ src/radix.py
@@ -2,33 +2,38 @@
 
 
 def radix_sort(unsorted, base=10):
-    """Sorts unsorted numbers in list working right- to left-most digit."""
+    """Sort unsorted numbers in list working right- to left-most digit."""
     holder_list = []
-    temp_list = []
-    buckets = [[] for _ in range(base)]
     nth_place = len(str(max(unsorted)))
-    iteration = base ** (nth_place - 1)
     if nth_place < 10:
         str_nth = '0' + str(nth_place)
     else:
         str_nth = str(nth_place)
     for num in unsorted:
         reformatted_num = format((num), str_nth)
-        holder_list.append(reformatted_num) #  all numbers same length, stringified with zeros in front
+        holder_list.append(reformatted_num)  #  all numbers same length, stringified with zeros in front
 
-    nth_place = int(nth_place)
-    while int(nth_place) > 0:
+    new_nth_place = int(nth_place)
+    while int(new_nth_place) > 0:
 
+        buckets = [[] for _ in range(base)]
+        temporary = buckets
+        iteration = base ** int(new_nth_place - 1)
         for num in holder_list:
-            nearly_sort = num // (base ** (int(nth_place) - 1)
+            nearly_sort = int(num) // iteration
             placement = str(nearly_sort)[-1]
-            buckets[int(placement) - 1].append(num)
+            import pdb; pdb.set_trace()
+            temporary[int(placement)].append(num)
 
-            for bucket in buckets:
-
-            holder_list = []
-
-        int(nth_place) -= 1
+        #     for bucket in buckets:
+        #         temp_list = []
+        #         for i in bucket:
+        #             if bucket[i]:
+        #                 returned_item = bucket.pop()
+        #                 temp_list = []
+        #                 temp_list.append(returned_item)
+        #     holder_list = temp_list
+        # new_nth_place -= 1
 
 
 unsorted = [170, 45, 75, 90, 2, 802, 2, 66]
