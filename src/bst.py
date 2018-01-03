804a44ca2d351bf35f6bb59dc3300761d6796665
Marco Zangari <mjzangari@gmail.com>
2018-01-03 10:40:34 -0800

    working on fixing traversals

--- src/bst.py
+++ src/bst.py
@@ -99,7 +99,7 @@ class BST(object):
     def delete(self, val):
         """Delete node from binary search tree."""
         curr = self.search(val)  # curr = node to delete
-        parent = self.parent
+        parent = curr.parent
 
         if not curr.left and not curr.right:  # node has no children
             if curr.val < parent.val:
@@ -157,3 +157,78 @@ class BST(object):
                 target.parent = curr.parent
                 target.left = curr.left
                 target.right = curr.right
+
+    def bst_pre_order_traversal(self):
+        """Traverse a binary search tree with pre-order."""
+        stack = []
+        curr = self.root
+        while stack or curr:
+            if node is curr:
+                curr = stack.pop()
+            else:
+                yield curr.val
+                stack.extend([curr.right, curr.left])
+                curr = stack.pop()
+
+    def bst_post_order_traversal(self):
+        """Traverse a binary search tree with post order."""
+        stack = []
+        curr = self.root
+        while curr or stack:
+            if curr:
+                if curr.right:
+                    stack.append(curr.right)
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                if stack and (curr.right == stack):
+                    stack.pop()
+                    stack.append(curr)
+                    curr = curr.right
+                else:
+                    yield curr.val
+                    curr = None
+
+    def bst_in_order_traversal(self):
+        """Traverse a binary search tree with in order sequence."""
+        stack = []
+        curr = None
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                yield curr.val
+                curr = curr.right
+
+    # def bst_breadth_first_traversal(self):
+    #     """Traverse binary search tree with breadth first sequence."""
+    #     stack = []
+    #     curr = self.root
+    #     while stack or curr:
+    #         if curr:
+    #             yield curr
+    #             stack.extend(curr.left, curr.right)
+    #             curr = stack.pop(0)
+    #         else:
+    #             curr = stack.pop(0)
+
+    def bst_breadth_first_traversal(self):
+        """Traverse a binary search tree via breadth first."""
+        current = self.root
+        nodes = []
+        nodes.append(current)
+        path = []
+        while nodes:
+            if current.left:
+                left = current.left
+                nodes.append(left)
+            if current.right:
+                right = current.right
+                nodes.append(right)
+            path.append(current)
+            nodes.delete(current)
+            current = nodes[0]
+        yield path
