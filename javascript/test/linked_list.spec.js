5647ff3dbf96d0e59a6f8805ce54bedd29c2b17a
Marco Zangari <mjzangari@gmail.com>
2018-01-04 13:17:22 -0800

    one working test

--- javascript/test/linked_list.spec.js
+++ javascript/test/linked_list.spec.js
@@ -10,5 +10,9 @@ describe('linked_list.js tests', function(){
     let ll = new linkedList.LinkedList()
     expect(ll.head).to.equal(null)
   })
-
+  it('test for linked list node empty', function(){
+    let ll = new linkedList.LinkedList()
+    expect(ll.head).is.null
+  })
+  it('test')
 })
