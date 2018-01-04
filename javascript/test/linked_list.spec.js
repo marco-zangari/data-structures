cdd5591da02555380bf01a5728c643d2f96fe5b8
Marco Zangari <mjzangari@gmail.com>
2018-01-04 12:30:02 -0800

    added test chai mochas

---
+++ javascript/test/linked_list.spec.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let linkedList = require('../linked_list')
+let chai = require('chai')
+let expect = chai.expect
+let assert = chai.assert
+
+describe(linked_list.js tests, function(){
+  it('test for linked list node', function(){
+    ll = linkedList.LinkedList()
+    ll.assert.isNull(ll.head, null)
+  })
+
+})
