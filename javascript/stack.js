30e21bb1bbc3273760635f4aed5b7eea09212c71
Marco Zangari <mjzangari@gmail.com>
2018-01-04 11:40:20 -0800

    added stack in js

--- javascript/stack.js
+++ javascript/stack.js
@@ -1,2 +1,27 @@
 'use strict'
 
+const LinkedList = require ('./linked_list')
+
+class Stack {
+    constructor(iterable=null){
+        this.func = new linkedlist.LinkedList()
+        if(Array.isArray(iterable)){
+            iterable.map(x => this.push(x));
+        }
+    }
+
+push(val){
+    return this.func.push(val)
+}
+
+pop(){
+    return this.func.pop()
+}
+
+size(){
+    return this.func.size()
+}
+
+}
+
+module.exports = Stack
