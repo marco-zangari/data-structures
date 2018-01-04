e026024a1929157b541825ef9a211fee15a91d41
Marco Zangari <mjzangari@gmail.com>
2018-01-04 13:31:13 -0800

    fixing heap; working through comments

--- src/max_heap.py
+++ src/max_heap.py
@@ -28,47 +28,43 @@ class MaxHeap(object):
         else:
             raise TypeError('Must add an integer')
 
-    def parent(self, i):
+    def _parent(self, i):
         """Return index of parent node."""
         return (i - 1) // 2
 
     def _sort(self, i):
         """Sort the heap."""
         n_idx = i
-        p_idx = self.parent(i)
+        p_idx = self._parent(i)
         if self._heap[n_idx] > self._heap[p_idx]:
             tmp = self._heap[i]
-            self._heap[n_idx] = self._heap[p_idx]
             self._heap[n_idx] = self._heap[p_idx]
             self._heap[p_idx] = tmp
             if p_idx:
                 self._sort(p_idx)
-            else:
-                return
-        else:
-            return
 
     def pop(self):
         """Pop the top value and resort the heap according to precedence."""
         try:
             top_node = self._heap[0]
             self._heap = [self._heap[-1]] + self._heap[1:-1]
-            self.sort_down(0)
+            self._sort_down()
             return top_node
         except IndexError:
             raise IndexError('Cannot pop from an empty heap')
 
-    def sort_down(self, i):
+    def _sort_down(self):
         """Sort after a pop."""
+        i = 0
         while ((i + 1) * 2) <= len(self._heap) + 1:
-            mc = self.max_child(i)
+            mc = self._max_child(i)
             if self._heap[i] < self._heap[mc]:
                 tmp = self._heap[i]
                 self._heap[i] = self._heap[mc]
                 self._heap[mc] = tmp
             i = mc
 
-    def max_child(self, i):
+    def _max_child(self, i):
         """Return the max child."""
         if (i * 2) + 2 > len(self._heap):
             return (i * 2) + 1
