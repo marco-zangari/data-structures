48b6ab8a9f693111331f21d6139cd9ff9405e9f5
Marco Zangari <mjzangari@gmail.com>
2018-01-05 11:08:14 -0800

    test for push and pop of empty stack

--- javascript/test/stack.spec.js
+++ javascript/test/stack.spec.js
@@ -9,4 +9,10 @@ describe('stack.js tests', function() {
     let stack = new Stack()
     expect(stack.size()).to.equal(0)
   })
+  it('test push and pop', function(){
+    let stack = new Stack()
+    stack.push('one')
+    stack.push('two')
+    expect(stack.pop()).is.equal('two')
+  })
 })
